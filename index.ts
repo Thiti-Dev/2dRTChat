c99283ea1dae1468f1fefcfd45e003c572a49560
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-02-07 10:02:18 +0700

    finalizing entry

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 /// <reference path="node_modules/webpack-dev-server/types/lib/Server.d.ts"/>
 import type { Configuration } from "webpack";
+import './style.scss';
 
+import {Application,Loader} from 'pixi.js';
+import * as PIXI from 'pixi.js';
+import { setupPixiApplication } from "./src/setup";
+
+
+// Get the div container
+const pixiContainer = document.getElementById('main-pixi-container');
+
+if(pixiContainer){
+    // Create a new Pixi application
+    setupPixiApplication(pixiContainer)
+}
