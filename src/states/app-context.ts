526db03a979eb67caa1b10e3b016294a91a7ae98
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-02-09 15:23:20 +0700

    :sparkles: Infinite world logics has been implemented, :zap: refactored code

--- src/states/app-context.ts
+++ src/states/app-context.ts
@@ -1,19 +1,23 @@
 import { observable, computed, action, flow } from "mobx"
-import type { Application } from "pixi.js"
+import type { Application, Container } from "pixi.js"
 import type { Socket } from "socket.io-client";
 import { Character } from "../classes/character";
+import world from "./world";
 
 class AppContext {
     private pixiApp!:Application<HTMLCanvasElement>;
     private socketInstance!: Socket
     private protagonist!: Character
     private nameTag!: string
+    private worldContainer!: Container
 
     constructor() {}
 
-    public setPixiApplication(app:Application<HTMLCanvasElement>){
-        console.log("[Trace]: Pixi Application has been stored into AppContext")
+    public setPixiApplication(app:Application<HTMLCanvasElement>,worldC: Container){
+        console.log("[Trace]: Pixi Application & World has been stored into AppContext")
         this.pixiApp = app
+        this.worldContainer = worldC
+
     }
 
     public setSocketInstance(s: Socket){
@@ -24,6 +28,9 @@ class AppContext {
     public setProtagonist(p: Character){
         console.log("[Trace]: Protagonist has been stored into AppContext")
         this.protagonist = p
+
+        // After setting up protagonist -> create world ticker to dynamically slide the tilingBackground when moves to edges
+        world.setupWorldUpdateTickerHandler(this.pixiApp, this.worldContainer, p)
     }
 
 
@@ -49,6 +56,11 @@ class AppContext {
     @computed
     public getNameTag(): string{
         return this.nameTag
+    }
+
+    @computed
+    public getWorldContainer(): Container{
+        return this.worldContainer
     }
 }
 
